b0d5c5ff785676d246ed0fbbe70e7f7724bedba6
rosbeldev <jorgerosbelcruzarias@gmail.com>
2025-12-08 19:39:55 +0100

    ✨ feat: add collection pagination and update metadata components

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,21 +1,20 @@
-import { checkTsConfig } from "@/utils/check_tsconfig"
-import { checkDependencies } from "@/utils/check_dependencies";
-import { writeFile, mkdir } from "fs/promises";
-import { join } from "path";
-import { folderExists } from "@/utils/folder_exists";
-import { fail, } from "@/utils/logs";
-import { addGlobalsCss } from "@/utils/add_globals_css";
-import chalk from "chalk";
-import boxen from "boxen";
-
+import { checkTsConfig } from '@/utils/check_tsconfig';
+import { checkDependencies } from '@/utils/check_dependencies';
+import { writeFile, mkdir } from 'fs/promises';
+import { join } from 'path';
+import { folderExists } from '@/utils/folder_exists';
+import { fail } from '@/utils/logs';
+import { addGlobalsCss } from '@/utils/add_globals_css';
+import chalk from 'chalk';
+import boxen from 'boxen';
 
 export const warnImportGlobalCss = () => {
   const message =
-    chalk.yellow("Import the ") +
-    chalk.magenta("globals.css") +
-    chalk.yellow(" file in any layout or page where you use UI Elements components.\n") +
-    chalk.yellow("We recommend adding it to a shared layout for consistent styling.\n\n") +
-    chalk.cyan("Example (Astro layout):\n") +
+    chalk.yellow('Import the ') +
+    chalk.magenta('globals.css') +
+    chalk.yellow(' file in any layout or page where you use UI Elements components.\n') +
+    chalk.yellow('We recommend adding it to a shared layout for consistent styling.\n\n') +
+    chalk.cyan('Example (Astro layout):\n') +
     chalk.gray(`---
 import "../styles/global.css";
 ---`);
@@ -23,57 +22,82 @@ import "../styles/global.css";
   const box = boxen(message, {
     padding: 1,
     margin: { top: 1, bottom: 1 },
-    borderColor: "yellow",
-    borderStyle: "round",
+    borderColor: 'yellow',
+    borderStyle: 'round',
   });
 
   console.log(box);
 };
 
 export const init = async () => {
+  try {
+    await checkTsConfig();
+    console.log(chalk.green('✔ Successfully checked ') + chalk.magenta('tsconfig.json'));
 
-    try {
+    await checkDependencies();
+    console.log(chalk.green('✔ Successfully checked ') + chalk.magenta('dependencies'));
 
-        await checkTsConfig();
-        console.log(
-            chalk.green("✔ Successfully checked ") +
-            chalk.magenta("tsconfig.json")
-        );
-
-
-        await checkDependencies();
-        console.log(
-            chalk.green("✔ Successfully checked ") +
-            chalk.magenta("dependencies")
-        );
-
-        const utils = `import { twMerge } from "tailwind-merge";
+    const utils = `import { twMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "clsx";
 
 export function cls(...args: ClassValue[]) {
     return twMerge(clsx(args))
-}`
+}
 
-        const libFolderPath = join(process.cwd(), "src/lib")
+import type { PaginateFunction } from 'astro';
+import { getCollection, type CollectionEntry } from 'astro:content';
 
-        if (!(await folderExists(libFolderPath))) {
-            await mkdir(libFolderPath)
+import type { collections } from '@/src/content/config';
+type CollectionName = keyof typeof collections;
+
+interface PaginationOptions {
+    pageSize?: number;
+    sortByFn?: ((a: CollectionEntry<CollectionName>, b: CollectionEntry<CollectionName>) => number);
+    filterFn?: (post: CollectionEntry<CollectionName>) => boolean;
+    collection: CollectionName
+}
+
+
+export function createCollectionPagination(options: PaginationOptions) {
+    const {
+        pageSize = 10,
+        sortByFn ,
+        filterFn,
+        collection
+    } = options;
+
+    return async ({ paginate }: { paginate: PaginateFunction }) => {
+        let items = await getCollection(collection);
+
+        if (filterFn) {
+            items = items.filter(filterFn);
         }
 
-        await writeFile(join(libFolderPath, "utils.ts"), utils)
+        if (sortByFn) {
+            items = items.sort(sortByFn);
+        } 
 
-        console.log(
-            chalk.green("✔ Successfully added ") +
-            chalk.magenta("lib/utils.ts")
-        );
+        return paginate(items, { pageSize });
+    };
+}
 
-        await addGlobalsCss()
+`;
 
-        warnImportGlobalCss()
-    } catch (error) {
-        fail(error as string);
-        process.exit(1);
+    const libFolderPath = join(process.cwd(), 'src/lib');
+
+    if (!(await folderExists(libFolderPath))) {
+      await mkdir(libFolderPath);
     }
 
+    await writeFile(join(libFolderPath, 'utils.ts'), utils);
 
-}
+    console.log(chalk.green('✔ Successfully added ') + chalk.magenta('lib/utils.ts'));
+
+    await addGlobalsCss();
+
+    warnImportGlobalCss();
+  } catch (error) {
+    fail(error as string);
+    process.exit(1);
+  }
+};
